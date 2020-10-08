0d5825a67211c19380c88ab8a1f56739376b6459
ssakethraj <ssakethraj@gmail.com>
2020-10-08 10:17:16 +0530

    add ip address and browser details

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,72 @@
 import React from 'react';
 import Head from 'next/head'
 import dynamic from 'next/dynamic';
+import {
+  browserName,
+  browserVersion,
+  deviceType,
+  osName,
+  osVersion
+} from "react-device-detect";
 import styles from '../styles/Home.module.css';
 const ChatBot = dynamic(import('../components/chatbot'), { ssr: false });
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Chat bot sandbox with NextJS</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <ChatBot />
-        <h1 className={styles.title}>
-          Chat bot sandbox with NextJS
-        </h1>
-      </main>
-    </div>
-  )
+class Home extends React.Component {
+  state = {
+    ip: {}
+  }
+  componentDidMount() {
+    this.setState({
+      iploading: true
+    })
+    fetch('https://ipapi.co/json/').then(res => res.json()).then(data => {
+      this.setState({
+        ip: data,
+        iploading: false
+      })
+      console.log(data)
+    }).catch(err => {
+      console.err(err);
+      this.setState({
+        iploading: false
+      })
+    })
+  }
+  render() {
+    const { ip, iploading } = this.state;
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Chat bot sandbox with NextJS</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className={styles.main}>
+          <ChatBot />
+          <div className={styles.grid}>
+            <div className={styles.card}>
+              <h2>Browser details</h2>
+              <p><b>Name: </b>{browserName}</p>
+              <p><b>Browser version: </b>{browserVersion}</p>
+              <p><b>Device type: </b>{deviceType}</p>
+              <p><b>Operating System: </b>{osName}</p>
+              <p><b>Operating System Version: </b>{osVersion}</p>
+            </div>
+            {!iploading ? <div className={styles.card}>
+              <h2>IP details</h2>
+              <p><b>IP address: </b>{ip.ip}</p>
+              <p><b>City: </b>{ip.city}</p>
+              <p><b>State: </b>{ip.region}</p>
+              <p><b>Country: </b>{ip.country_name}</p>
+              <p><b>Country Code:  </b>{ip.country}</p>
+              <p><b>Country calling code:  </b>{ip.country_calling_code}</p>
+              <p><b>Currency:  </b>{ip.currency}</p>
+              <p><b>Lat. and Long.:  </b>{ip.latitude}, {ip.longitude}</p>
+            </div> : <div className={styles.card} >Loading...</div>}
+          </div>
+        </main>
+      </div >
+    )
+  }
 }
+
+export default Home;
